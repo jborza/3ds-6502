bae022fc40c6c298f892eb934238ff3f16f4a217
jborza <jborza@gmail.com>
2019-06-01 21:31:31 +0200

    drawing first pixels

--- source/main.c
+++ source/main.c
@@ -1,28 +1,74 @@
+#include <citro2d.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <3ds.h>
+
+#include "color.h"
+
+#define SCREEN_WIDTH  400
+#define SCREEN_HEIGHT 240
+
+//for 32x32 we need 7x7 large pixels
+#define PIXEL_SIZE 7
+
+void C2D_DrawSolidSquare(float x, float y, float size, u32 color){
+    const float z = 0;
+    C2D_DrawRectangle(x, y, z, size, size, color, color, color, color);
+}
+
+void render(){
+    for(int x = 0; x < 32; x++){
+        C2D_DrawSolidSquare(x*7, x*7, PIXEL_SIZE, getColor(x));
+        // C2D_DrawRectangle(x*7, x*7, PIXEL_SIZE, PIXEL_SIZE, clrRed, clrRed, clrRed, clrRed);
+    }
+}
 
 int main(int argc, char* argv[])
 {
 	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
+    C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+	C2D_Prepare(); 
 
+    // Create screens
+	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
+	consoleInit(GFX_BOTTOM, NULL);
+
+    //create colors
+    initializePalette();
+    // Create colors
+    u32 clrClear = palette[0];
+ 
 	printf("Hello, world!\n");
 
 	// Main loop
 	while (aptMainLoop())
 	{
-		gspWaitForVBlank();
-		gfxSwapBuffers();
+		// gspWaitForVBlank();
+		// gfxSwapBuffers();
 		hidScanInput();
 
-		// Your code goes here
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
 			break; // break in order to return to hbmenu
+
+        //diagnostics
+        printf("\x1b[2;1HCPU:     %6.2f%%\x1b[K", C3D_GetProcessingTime()*6.0f);
+		printf("\x1b[3;1HGPU:     %6.2f%%\x1b[K", C3D_GetDrawingTime()*6.0f);
+		printf("\x1b[4;1HCmdBuf:  %6.2f%%\x1b[K", C3D_GetCmdBufUsage()*100.0f);
+
+        // Render the scene
+		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+		C2D_TargetClear(top, clrClear);
+		C2D_SceneBegin(top); 
+
+        render();
+
+        C3D_FrameEnd(0); 
 	}
 
+	C2D_Fini();
+	C3D_Fini();
 	gfxExit();
 	return 0;
 }
